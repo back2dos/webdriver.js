9af1573fb064530cc32b2124ec951dc20baa7963
Juraj Kirchheim <back2dos@googlemail.com>
2016-01-29 16:28:45 +0100

    Add when to promise.

--- src/webdriver/Promise.hx
+++ src/webdriver/Promise.hx
@@ -4,4 +4,5 @@ import tink.core.Callback;
 
 interface Promise<T> {
   function then(handler:Callback<T>):Promise<T>;
+  function when<A>(map:T->A):Promise<A>;
 }
--- src/webdriver/WebElementPromise.hx
+++ src/webdriver/WebElementPromise.hx
@@ -3,4 +3,5 @@ import tink.core.Callback;
 
 extern class WebElementPromise extends WebElement implements Promise<WebElement> {
   function then(handler:Callback<WebElement>):WebElementPromise;
+  function when<A>(map:WebElement->A):Promise<A>;
 }
